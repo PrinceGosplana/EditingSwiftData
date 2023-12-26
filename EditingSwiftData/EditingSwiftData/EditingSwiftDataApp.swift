50718dd0812117377590fe9ca8dd317e01e1ba06
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-26 10:10:14 +0200

    init project

---
+++ EditingSwiftData/EditingSwiftData/EditingSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditingSwiftDataApp.swift
+//  EditingSwiftData
+//
+//  Created by OLEKSANDR ISAIEV on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct EditingSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
