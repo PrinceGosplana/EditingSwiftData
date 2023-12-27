5e79ebc59ce7113d5bd312ff4a880f633667f10f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-27 06:04:59 +0200

    make filter more complicated

--- EditingSwiftData/EditingSwiftData/ContentView.swift
+++ EditingSwiftData/EditingSwiftData/ContentView.swift
@@ -12,8 +12,17 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query(filter: #Predicate<User> { user in
 //        user.name.contains("R") // will find only capitalized R
-        user.name.localizedStandardContains("R") &&// R or r
-        user.city == "London"
+//        user.name.localizedStandardContains("R") &&// R or r
+//        user.city == "London"
+        if user.name.localizedStandardContains("R") {
+            if user.city == "London" {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
     }, sort: \User.name) var users: [User]
     @State private var path = [User]()
     
